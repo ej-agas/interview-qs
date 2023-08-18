24674862bde1f681b0dda61d1b795a5f48e4edeb
ej-agas <agas.ej09@gmail.com>
2023-08-18 14:42:52 +0800

    add tests
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Interview-qs
+# interview-qs
+
+![Tests](https://github.com/ej-agas/interview-qs/actions/workflows/tests.yaml/badge.svg) [![Go Report Card](https://goreportcard.com/badge/github.com/ej-agas/interview-qs)](https://goreportcard.com/report/github.com/ej-agas/interview-qs)
 
 My solutions to technical interview questions that I have been asked
--- palindrome.go
+++ palindrome.go
@@ -6,7 +6,7 @@ func IsPalindrome(s string) bool {
 	// transform to lowercase the string and remove all spaces
 	s = strings.ReplaceAll(strings.ToLower(s), " ", "")
 
-	for i := 0; i < len(s)/2; i++ { // loop until the middle part of the string
+	for i := 0; i < len(s)/2; i++ { // run loop until the middle part of the string
 		left := s[i]           // left side of the string
 		right := s[len(s)-1-i] // right side of the string
 
